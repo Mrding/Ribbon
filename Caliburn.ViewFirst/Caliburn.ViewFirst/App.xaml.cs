2bab14fb2269f604cf707708c80beace8585082b
Mrding <dingli_89757@gmail.com>
2011-09-20 17:18:46 +0800

    add caliburn

---
+++ Caliburn.ViewFirst/Caliburn.ViewFirst/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Caliburn.ViewFirst {
+    using System.Windows;
+
+    public partial class App : Application {
+        public App() {
+            new MefBootstrapper();
+            InitializeComponent();
+        }
+    }
+}
